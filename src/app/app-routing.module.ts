2c7e9e5bda8da3a39db9e612e628047cda28f3af
Ricardo Bastardo <bastvai@gmail.com>
2023-03-17 16:48:14 -0400

    first cmmit with basic design router and pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { ChartsComponent } from './pages/charts/charts.component';
+import { NonPageFoundComponent } from './pages/non-page-found/non-page-found.component';
+import { PagesComponent } from './pages/pages.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: '', 
+    component: PagesComponent,
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'progress', component: ProgressComponent },
+      { path: 'charts', component: ChartsComponent },
+      { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+      
+    ] 
+  },
+
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  
+  
+  { path: '**', component: NonPageFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/pages/non-page-found/non-page-found.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-non-page-found',
+  templateUrl: './non-page-found.component.html',
+  styles: [
+  ]
+})
+export class NonPageFoundComponent {
+  year = new Date().getFullYear()
+
+}
